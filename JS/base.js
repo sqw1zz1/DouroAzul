b731ace3748dbf1c0ef0b27a1fe82b08d78be4d0
sqw1zz1 <59416111+sqw1zz1@users.noreply.github.com>
2022-10-27 21:41:16 +0100

    abc

--- JS/base.js
+++ JS/base.js
@@ -1,9 +1,50 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+    ShowHeader();
+    GetFleet();
+});
+
+function ShowHeader() {
     $.ajax({
-        url: "/header.html",
+        url: "/components/header.html",
         type: "GET",
-        success: function(data) {
+        success: function (data) {
             $("#header").empty().append(data);
         }
     });
-});
+}
+
+function GetFleet() {
+    $.ajax({
+        url: "/JS/objects/fleet.json",
+        type: "GET",
+        success: function (data) {
+            for (var i in data) {
+                var fleet = data[i];
+                SetItem(fleet.name, fleet.description, fleet.url_image);
+            }
+        }
+    });
+}
+
+function GetDestinations() {
+    $.ajax({
+        url: "/JS/objects/destinations.json",
+        type: "GET",
+        success: function (data) {
+            for (var i in data) {
+                var destination = data[i];
+                SetItem(destination.name, destination.description, destination.url_image);
+            }
+        }
+    });
+}
+
+function SetItem(name, description, urlImage) {
+    $.ajax({
+        url: "/components/Item.html",
+        type: "GET",
+        success: function (data) {
+            $(".presentation-container").append(data.replace("{param.item}", name))
+        }
+    });
+}
