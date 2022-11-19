12df9b6c4c4b59b31ffef6ec70791dfe203a2283
sqw1zz1 <59416111+sqw1zz1@users.noreply.github.com>
2022-11-19 15:27:49 +0000

    Added Travell screens

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     GetDestinations();
+    getTravelingList();
 });
 
 function GetDestinations() {
@@ -37,4 +38,37 @@ function GetItem(destinations, id) {
 function SetDestination(id) {
     document.cookie = "destination=" + id + ";";
     window.location.href = "destination.html";
+}
+
+function getTravelingList() {
+    $.ajax({
+        url: "JS/objects/traveling.json",
+        type: "GET",
+        success: function (data) {
+            getItemTravel(data, "traveling-container")
+        }
+    });
+}
+
+function getItemTravel(traveling, id) {
+    $.ajax({
+        url: "components/Item-travel.html",
+        type: "GET",
+        success: function (data) {
+            var sizeData = traveling.length;
+            if (sizeData > 4) {
+                sizeData = 4;
+            }
+
+            for (let i = 0; i < sizeData; i++) {
+                $("#" + id).append(
+                    data.replace("{param.name}", traveling[i].name)
+                        .replace("{param.description}", traveling[i].description)
+                        .replace("{param.urlImage}", traveling[i].url_image)
+                        .replace("{param.click}", "SetDestination(" + traveling[i].id + ")")
+                        .replace("{param.price}", traveling[i].price)
+                );
+            }
+        }
+    });
 }
